f364f0dd001511c2d9b0fe24f3ea5612da14fea8
Ronald Abu Saleh <ronald.abusaleh@gmail.com>
2023-05-01 11:18:57 -0400

    Test for person class

---
+++ demos/src/06-person.test.js
@@ -0,0 +1,24 @@
+const Person = require('./06-person')
+
+describe('Test for Person', () => {
+
+  let person = null
+
+  beforeEach(() => {
+    person = new Person('Ronald', 45, 1.7)
+  })
+
+  test('should return down', () => {
+    person.weight = 45
+    const imc = person.calcIMC()
+
+    expect(imc).toBe('down')
+  })
+
+  test('should return normal', () => {
+    person.weight = 59
+    const imc = person.calcIMC()
+
+    expect(imc).toBe('normal')
+  })
+})
