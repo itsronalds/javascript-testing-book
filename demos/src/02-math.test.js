c943212d5fd8a879cd06f48106c6a0de1ad65bcb
Ronald Abu Saleh <ronald.abusaleh@gmail.com>
2023-05-01 11:19:15 -0400

    Added describe to math tests

--- demos/src/02-math.test.js
+++ demos/src/02-math.test.js
@@ -2,30 +2,35 @@ const {
   sum, multiply, divide, average,
 } = require('./02-math');
 
-test('adds 1 + 3 should be 4', () => {
-  const rta = sum(1, 3);
-  expect(rta).toBe(4);
-});
+describe('Test for math', () => {
+  describe('tests for sum', () => {
+    test('adds 1 + 3 should be 4', () => {
+      const rta = sum(1, 3);
+      expect(rta).toBe(4);
+    });
+  });
+  describe('tests for multiply', () => {
+    test('multiplication should be 4', () => {
+      const rta = multiply(2, 2);
+      expect(rta).toBe(4);
+    });
+  });
+  describe('tests for division', () => {
+    test('divisions', () => {
+      const rta = divide(6, 3);
+      expect(rta).toBe(2);
 
-test('multiplication should be 4', () => {
-  const rta = multiply(2, 2);
-  expect(rta).toBe(4);
-});
+      const rta2 = divide(9, 3);
+      expect(rta2).toBe(3);
+    });
+    test('should divide for zero', () => {
+      const rta = divide(6, 0);
+      expect(rta).toBeNull();
 
-test('divisions', () => {
-  const rta = divide(6, 3);
-  expect(rta).toBe(2);
-
-  const rta2 = divide(9, 3);
-  expect(rta2).toBe(3);
-});
-
-test('should divide for zero', () => {
-  const rta = divide(6, 0);
-  expect(rta).toBeNull();
-
-  const rta2 = divide(10, 0);
-  expect(rta2).toBeNull();
+      const rta2 = divide(10, 0);
+      expect(rta2).toBeNull();
+    });
+  })
 });
 
 test('should be array of notes (numbers) between 0 and 10', () => {
